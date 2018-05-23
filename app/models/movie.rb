2e57a785628bc9b78d5bd2862923499f3e25ad55
Marta Kosmala <marta.kos1230@gmail.com>
2018-05-23 22:08:09 +0200

    add validation for movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :directed_by, presence: true
+  validates :written_by, presence: true
+  validates :release_date, presence: true
+  validates :length, presence: true
+  validates :genre, presence: true
+  validates :country, presence: true
+  validates :description, presence: true,
+                          length: { minimum: 20 }
 end
