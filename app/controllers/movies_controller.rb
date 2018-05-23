30956b48eb1687f2d246c3f6d6ff730f7eb18e5e
Marta Kosmala <marta.kos1230@gmail.com>
2018-05-23 21:37:59 +0200

    Add movie new and show views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,20 @@
 class MoviesController < ApplicationController
-  def new
+  def index
+    @movies = Movie.all
   end
 
   def create
-    @movie = Movie.new(params[:movie])
+    @movie = Movie.new(movie_params)
 
     @movie.save
     redirect_to @movie
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new; end
 
   private
     def movie_params
